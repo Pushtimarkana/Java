e302d25fc4ae1eb92b2fdd8d1487070a8b0ce339
Pushtimarkana <pushtimarkana@gmail.com>
2024-06-22 21:49:33 +0530

    CommandLine

---
+++ Lab1/Lab1p4.java
@@ -0,0 +1,10 @@
+public class Lab1p4{
+	public static void main(String[] args) {
+
+		// Scanner sc=new Scanner(System.in);
+		double n1=Double.parseDouble(args[0]);
+		double n2=Double.parseDouble(args[1]);
+		double n=n1+n2;
+		System.out.println(n);
+	}
+}
