081cf2e523995dc20f4b04839f13f2939425cfa7
Pushtimarkana <pushtimarkana@gmail.com>
2024-06-24 23:28:06 +0530

    CommondLine_argument

---
+++ Lab2/Lab2p1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Lab2p1{
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		double n1=Double.parseDouble(args[0]);
+		double n2=Double.parseDouble(args[1]);
+		double n=n1+n2;
+		System.out.println(n);
+
+		System.out.println("Enter a number");
+		int n3=sc.nextInt();
+		System.out.println("Enter a number");
+		int n4=sc.nextInt();
+		int n5=n3+n4;
+		System.out.println(n5);
+
+	}
+}
