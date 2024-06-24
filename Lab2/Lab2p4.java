a124f9efa06193bed8fcea5974ec15c5a6855158
Pushtimarkana <pushtimarkana@gmail.com>
2024-06-24 23:34:05 +0530

    Area_Circle

---
+++ Lab2/Lab2p4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Lab2p4{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter radius for circle");
+		double r = sc.nextDouble();
+		double a = 3.14*r*r;
+		System.out.println("area="+a);
+
+	}
+}
